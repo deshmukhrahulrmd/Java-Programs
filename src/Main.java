18a5e8ec7226cad0654723d0ea2c2eb15f9d695b
Rahul Deshmukh <deshmukhrahulrmd@gmail.com>
2024-07-05 18:39:42 +0530

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,8 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
 
+        for(int i=0;i<10;i++) {
+            System.out.println("suiiii");
+        }
     }
 }
